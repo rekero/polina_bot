22a1a1ff23c2ed97c3bab160a58e9aa4ffacfd8a
Your Name <you@example.com>
2025-11-11 17:26:24 +0100

    add gotquestion zapas

--- main.rb
+++ main.rb
@@ -27,6 +27,8 @@ CHGK_IMAGE_URL = 'https://db.chgk.info/images/db/'
 CHGK_COPYRIGHT_URL = 'http://db.chgk.net'
 PIC_TEXT = 'pic: '
 DUDKIN = 110064
+ZAPAS_URL = 'https://gotquestions.online/'
+ZAPAS_QUESTION_URL = 'https://gotquestions.online/question/'
 
 def good_boy(bot)
   @time = Time.now + Random.rand(146000..176000)
@@ -68,21 +70,26 @@ def work(bot)
   begin
     bot.listen do |message|
       p message
-      case message.text
-      when '/stats@the_polina_bot'
+      case message.text.to_s.split("@").first
+      when '/stats'
 	bot.api.send_message(chat_id: message.chat.id, text: users.map { |k, v| "#{k} : #{v}" }.join(';'))
-      when '/horosh@the_polina_bot'
+      when '/horosh'
         bot.api.send_sticker(chat_id: message.chat.id, sticker: HOROSH_STICKER)
-      when '/reaction@the_polina_bot'
+      when '/reaction'
         stickers = bot.api.get_sticker_set(name: EBANINA_STICKER_PACK)['result']['stickers']
         bot.api.send_sticker(chat_id: message.chat.id, sticker: stickers.sample['file_id'])
-      when '/est_cho@the_polina_bot'
+      when '/est_cho'
         bot.api.send_message(chat_id: message.chat.id, text: "#чёпосмотреть #чёпочитать")
-      when '/dudkin@the_polina_bot'
+      when '/dudkin'
         vk = VkontakteApi::Client.new
         jokes = vk.wall.get(owner_id: DUDKIN, count: 100, filter: 'owner', access_token: VK_TOKEN, v: '5.103', offset: Random.rand(0..9)*100)[:items]
         bot.api.send_message(chat_id: message.chat.id, text: jokes.sample[:text])
-      when '/question@the_polina_bot'
+      when '/zapas'
+        question, answer = parse_zapas_question
+        bot.api.send_message(chat_id: message.chat.id, text: question)
+        sleep 65
+        bot.api.send_message(chat_id: message.chat.id, text: answer)
+      when '/question'
 	if message.chat.username == 'vprsk'
           name = message.from.username || "#{message.from.first_name} #{message.from.last_name}"
           unless  users[name].nil?
@@ -119,6 +126,25 @@ def parse_question
   tour = Nokogiri::XML(request).at_xpath('//ParentTextId').content
   number = Nokogiri::XML(request).at_xpath('//Number').content
   return "#{clean_text(question)} #{date}", "#{clean_text(answer)}(#{criteria})(#{clean_text(comments)}) #{CHGK_COPYRIGHT_URL}/question/#{tour}/#{number}", authors
+end
+
+def parse_zapas_question
+  uri = URI("#{ZAPAS_QUESTION_URL}#{rand(1..410000)}")
+  p uri.to_s
+  request = Net::HTTP.get(uri)
+  result = if Nokogiri::HTML.parse(request).css("script")[37].content.include?('nezachet')
+    Nokogiri::HTML.parse(request).css("script")[37].content.delete('\\')
+  elsif Nokogiri::HTML.parse(request).css("script")[38].content.include?('nezachet')
+    Nokogiri::HTML.parse(request).css("script")[38].content.delete('\\')
+  end
+  return uri.to_s, "" if result.nil?
+
+  question = result.slice(result.index('text"')..result.index('","razdatkaText'))[7..-3]
+  answer = result.slice(result.index('answer"')..result.index('","answerPic'))[9..-3]
+  zachet = result.slice(result.index('zachet"')..result.index('nezachet'))[10..-4]
+  comment = result.slice(result.index('comment"')..result.index('note'))[10..-4]
+  razdatka = result.slice(result.index('razdatkaPic"')..result.index('audio'))[15..-4]
+  return "#{question} #{razdatka}", "#{answer} (#{zachet})(#{comment}) #{ZAPAS_URL}"
 end
 
 def clean_text(text)
